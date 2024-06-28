9e1496491c26450bc0b58375d99553174bb33037
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-06-28 19:38:30 +0000

    random nft picker edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,17 @@ document
 
 function showRandomImage() {
   const images = [
-    "assets/images/jeongyoeb-han-0012.jpg",
-    "assets/images/jeongyoeb-han-0014.jpg",
-    "assets/images/jeongyoeb-han-0015.jpg",
-    "assets/images/jeongyoeb-han-0025.jpg",
-    "assets/images/jeongyoeb-han-0048.jpg",
-    "assets/images/jeongyoeb-han-00488.jpg",
-    "assets/images/jeongyoeb-han-cyberfiction-pf-t-0111.jpg",
-    "assets/images/jeongyoeb-han-cyberfiction-pf-t-0222.jpg",
-    "assets/images/jeongyoeb-han-cyberfiction-pf-t-033.jpg",
-    "assets/images/jeongyoeb-han-cyberfiction-pf-t-0333.jpg",
+    "assets/images/1.png",
+    "assets/images/2.png",
+    "assets/images/3.png",
+    "assets/images/4.png",
+    "assets/images/5.png",
+    "assets/images/6.png",
+    "assets/images/7.png",
+    "assets/images/8.png",
+    "assets/images/9.png",
+    "assets/images/10.png",
+    "assets/images/11.png",
   ];
 
   const randomIndex = Math.floor(Math.random() * images.length);
